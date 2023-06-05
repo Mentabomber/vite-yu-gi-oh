66255babd513d7143fd1864d591543d36b08f318
Menta <simcictilen.gmail.com>
2023-06-05 17:18:59 +0200

    components creation

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script>
+
+
+export default{
+    name: "AppHeader"
+}
+</script>
+
+<template>
+    <header>
+        <div class="img-container">
+            <img src="Yu-Gi-Oh!_(Logo).jpg" alt="yu-gi-oh-logo">
+        </div>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header{
+    display: flex;
+    align-items: center;
+    align-self: center;
+    padding: 15px;
+}
+.img-container{
+
+    width: 150px;
+
+    img {
+        width: 100%;
+    }
+}
+
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,43 @@
+<script>
+import AppMainCards from './components/AppMainCards.vue';
+
+export default{
+    name: "AppMain",
+    components: {
+        AppMainCards
+    }
+}
+</script>
+
+<template>
+<main>
+    <div class="container">
+        <select name="cards" id="cards">
+            <option value="Alien">Alien</option>
+        </select>
+    
+    </div>
+</main>
+</template>
+
+<style lang="scss" scoped>
+main{
+    background-color: rgb(212,143,56);
+    
+    .container {
+        width: 1180px;
+        height: 1000px;
+        margin: auto;
+        background-color: white;
+
+        select {
+            margin-top: 20px;
+            padding: 10px;
+            padding-right: 100px;
+            padding-left: 5px;
+            
+            font-size: 20px;
+        }
+    }
+}
+</style>
